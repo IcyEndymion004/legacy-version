d8e070f76bd70f0d27a8b620ba3118de6b7e2801
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2026-01-24 03:22:29 -0600

    Forgor to add

--- legacyver/all.go
+++ legacyver/all.go
@@ -21,6 +21,7 @@ var (
 // must be set to true if you're using Dragonfly.
 func All(dragonflyMapping bool) []minecraft.Protocol {
 	return []minecraft.Protocol{
+		New860(dragonflyMapping),
 		New859(dragonflyMapping),
 		New844(dragonflyMapping),
 		New827(dragonflyMapping),
