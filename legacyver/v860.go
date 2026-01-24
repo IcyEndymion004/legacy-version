eabc7f727711f16f4de09cf8367ab8dcf526d736
IcyEndymion004 <63156236+IcyEndymion004@users.noreply.github.com>
2026-01-24 00:52:26 -0800

    Revert back to legacy system (testing)

--- legacyver/v860.go
+++ legacyver/v860.go
@@ -1,12 +1,33 @@
 package legacyver
 
 import (
+	_ "embed"
+
 	"github.com/akmalfairuz/legacy-version/legacyver/proto"
+	"github.com/akmalfairuz/legacy-version/mapping"
+)
+
+const (
+	// ItemVersion860 ...
+	ItemVersion860 = 251
+	// BlockVersion860 ...
+	BlockVersion860 int32 = (1 << 24) | (21 << 16) | (120 << 8)
+)
+
+var (
+	//go:embed data/required_item_list_859.json
+	requiredItemList860 []byte
+	//go:embed data/block_states_859.nbt
+	blockStateData860 []byte
 )
 
 func New860(dragonflyMapping bool) *Protocol {
-	p := New859(dragonflyMapping)
-	p.ver = "1.21.124"
-	p.id = proto.ID860
-	return p
+	itemMapping := mapping.NewItemMapping(requiredItemList860, ItemVersion860)
+	blockTranslator := lookupOrCreateBlockTranslator(860, BlockVersion860, blockStateData860)
+	return &Protocol{
+		ver:             "1.21.124",
+		id:              proto.ID860,
+		blockTranslator: blockTranslator,
+		itemTranslator:  NewItemTranslator(itemMapping, itemMappingLatest(dragonflyMapping), blockTranslator.BlockMapping(), blockMappingLatest),
+	}
 }
